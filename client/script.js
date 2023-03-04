b486cca65fba00b6137823a316c751010207681c
asma-mo <Asom13_@outlook.com>
2023-03-04 11:53:05 +0300

    message

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handleSubmit = async (e) => {
   
   loader(messageDev);
 
-  const response = await fetch("http://localhost:9000",{
+  const response = await fetch("https://ai-chat-n1im.onrender.com",{
     method: "POST",
     headers: {
       "Content-Type": "application/json"
